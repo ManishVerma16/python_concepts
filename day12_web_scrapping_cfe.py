d85e60a3569c90a902f1c8b8af18acd32bf2b8c4
Manish Verma <vermam318@gmail.com>
2021-06-21 12:31:12 +0530

    final script for scrapping the data from boxofficemojo website

--- day12_web_scrapping_cfe.py
+++ day12_web_scrapping_cfe.py
@@ -1,4 +1,5 @@
 import requests, datetime
+import pandas as pd
 from requests_html import HTML
 
 now = datetime.datetime.now()
@@ -18,7 +19,7 @@ def url_to_txt(url, filename='world.html', save=False):
 
 url = 'https://www.boxofficemojo.com/year/world/'
 
-html_text = url_to_txt(url, save=True)
+html_text = url_to_txt(url)
 
 r_html = HTML(html=html_text)
 
@@ -26,7 +27,30 @@ table_class = '.imdb-scroll-table'
 
 r_table = r_html.find(table_class)
 
-print(r_table)
+# print(r_table)
+# print(r_table[0].text)
+
+table_data = []
+header_names = []
+if len(r_table) == 1:
+    parsed_table = r_table[0]
+    rows = parsed_table.find('tr')
+    header_row = rows[0]
+    header_cols = header_row.find('th')
+    header_names = [x.text for x in header_cols]
+    print(header_names)
+    for row in rows[1:]:
+        cols = row.find('td')
+        row_data = []
+        for i, col in enumerate(cols):
+            # print(i, col.text, '\n')
+            row_data.append(col.text)
+        table_data.append(row_data)
+
+# print(table_data)
+
+df = pd.DataFrame(table_data, columns=header_names)
+df.to_csv('movies_collection.csv', index=False)
 
 
 
